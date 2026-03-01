acb067e1bd539de4eef1f0fbcbda57196dba4e54
broccoli-robbie <broccolirobbie@proton.me>
2026-03-01 08:28:23 -0500

    add extractor with tests

---
+++ src/test_extractor.py
@@ -0,0 +1,30 @@
+import unittest
+
+from extractor import extract_markdown_images
+from extractor import extract_markdown_links
+
+
+class TestExtractor(unittest.TestCase):
+    def test_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertListEqual(
+            extract_markdown_images(text),
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+        )
+
+    def test_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertListEqual(
+            extract_markdown_links(text),
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
