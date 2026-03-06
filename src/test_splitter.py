0e619ced54140faa4d635465e6bf4cf8a6d6cf2e
broccoli-robbie <broccolirobbie@proton.me>
2026-03-05 22:40:07 -0500

    add link and image splitter functions with more tests

--- src/test_splitter.py
+++ src/test_splitter.py
@@ -1,6 +1,8 @@
 import unittest
 
 from splitter import split_nodes_delimiter
+from splitter import split_nodes_image
+from splitter import split_nodes_link
 from textnode import TextNode
 from textnode import TextType
 
@@ -36,6 +38,48 @@ class TestDelimiter(unittest.TestCase):
                 TextNode("This is text with an ", TextType.TEXT),
                 TextNode("italic", TextType.ITALIC),
                 TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+
+class TestImages(unittest.TestCase):
+    def test_split_images(self):
+        old_nodes = [
+            TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                TextType.TEXT,
+            )
+        ]
+        self.assertListEqual(
+            split_nodes_image(old_nodes),
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+        )
+
+
+class TestLinks(unittest.TestCase):
+    def test_split_links(self):
+        old_nodes = [
+            TextNode(
+                "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+                TextType.TEXT,
+            )
+        ]
+        self.assertListEqual(
+            split_nodes_link(old_nodes),
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
             ],
         )
 
